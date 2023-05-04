3419d8b502e1d1f58b1056be141331ad23f9b2eb
Daniel Woodell <95502140+dWoodell@users.noreply.github.com>
2023-05-04 18:06:47 -0400

    Prayers
    Please

---
+++ Assets/Scripts/characterMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    public Rigidbody2D rb;
+
+    private float move;
+
+    public bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+
+        if (other.gameObject.CompareTag("Platform"))
+        {
+            isJumping = true;
+        }
+    }
+}
